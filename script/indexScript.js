0a81901e17d26613aa482298ddeaa0d60078e934
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-01-28 17:55:50 +0900

    1월 28일 클론코딩(메인페이지 완성 + 서브페이지시작 필요)

--- script/indexScript.js
+++ script/indexScript.js
@@ -5,6 +5,14 @@ var jobMain = document.getElementById("jobMain");
 var yellowBar1 = document.querySelector(".pressMainC");
 var yellowBar2 = document.querySelector(".jobMainC");
 
+var related_site_list = document.getElementById('link_list_box1');
+var fire_school_list = document.getElementById('link_list_box2');
+
+var closeBtn1 = document.getElementById('closeBtn1');
+var closeBtn2 = document.getElementById('closeBtn2');
+
+var related_site_list_title = document.getElementById('downtown_fire');
+var fire_school_list_title = document.getElementById('local_fire');
 
 pressTitle.addEventListener('click', () => {
     pressMain.style.display = 'block';
@@ -25,3 +33,23 @@ jobTitle.addEventListener('click', () => {
     yellowBar2.style.color = '#222';
     return false;
 }); // 보도자료 내용 변경을 위함
+
+
+related_site_list_title.addEventListener('click', () => {
+    related_site_list.style.display = 'block';
+})
+
+closeBtn1.addEventListener('click', () => {
+    related_site_list.style.display='none';
+    return false;
+});
+
+
+fire_school_list_title.addEventListener('click', () => {
+    fire_school_list.style.display = 'block';
+})
+
+closeBtn2.addEventListener('click', () => {
+    fire_school_list.style.display='none';
+    return false;
+});
