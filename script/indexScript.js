eda1200dc65a2b4b41739cb6a77ad4a5d4152e03
hooting76 <60126117+hooting76@users.noreply.github.com>
2022-02-21 18:19:38 +0900

    메인 sns아이콘 수정 + 배너 js 시작

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,18 +1,19 @@
+// 메인배너 기능
+
+
+
+
+
+
+
+
+// 보도자료 관련 변수
 var pressTitle = document.getElementById("pressTitle");
 var jobTitle = document.getElementById("jobTitle");
 var pressMain = document.getElementById("pressMain");
 var jobMain = document.getElementById("jobMain");
 var yellowBar1 = document.querySelector(".pressMainC");
 var yellowBar2 = document.querySelector(".jobMainC");
-
-var related_site_list = document.getElementById('link_list_box1');
-var fire_school_list = document.getElementById('link_list_box2');
-
-var closeBtn1 = document.getElementById('closeBtn1');
-var closeBtn2 = document.getElementById('closeBtn2');
-
-var related_site_list_title = document.getElementById('downtown_fire');
-var fire_school_list_title = document.getElementById('local_fire');
 
 pressTitle.addEventListener('click', () => {
     pressMain.style.display = 'block';
@@ -35,6 +36,14 @@ jobTitle.addEventListener('click', () => {
 }); // 보도자료 내용 변경을 위함
 
 
+// 푸터영역 관련사이트 목록기능
+var related_site_list = document.getElementById('link_list_box1');
+var fire_school_list = document.getElementById('link_list_box2');
+var closeBtn1 = document.getElementById('closeBtn1');
+var closeBtn2 = document.getElementById('closeBtn2');
+var related_site_list_title = document.getElementById('downtown_fire');
+var fire_school_list_title = document.getElementById('local_fire');
+
 related_site_list_title.addEventListener('click', () => {
     related_site_list.style.display = 'block';
 })
@@ -44,7 +53,6 @@ closeBtn1.addEventListener('click', () => {
     return false;
 });
 
-
 fire_school_list_title.addEventListener('click', () => {
     fire_school_list.style.display = 'block';
 })
@@ -53,3 +61,4 @@ closeBtn2.addEventListener('click', () => {
     fire_school_list.style.display='none';
     return false;
 });
+
